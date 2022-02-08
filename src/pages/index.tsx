3dc38269f5c27ba8e736dca110ae78af3d4c3abe
ingridflack <ingrid.flack@newtail.com.br>
2022-02-08 17:26:09 -0300

    languages json added and homepage updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import TextareaField from "../components/TextareaField";
+import TextareaField from "../components/Form/TextareaField";
 
-import { Container, TextareaBox } from "./styles";
+import languages from "../assets/languages.json";
+
+import { Container, Wrapper, TranslationGroup, Button } from "./styles";
+import Select from "../components/Form/Select";
+import { FaExchangeAlt } from "react-icons/fa";
 
 const Homepage = () => {
   return (
     <Container>
-      <TextareaBox>
-        <TextareaField />
-        <TextareaField />
-      </TextareaBox>
+      <Wrapper>
+        <TranslationGroup>
+          <Select options={languages} name="teste1" />
+          <TextareaField />
+        </TranslationGroup>
+        <Button>
+          <FaExchangeAlt />
+        </Button>
+        <TranslationGroup>
+          <Select options={languages} name="teste2" />
+          <TextareaField />
+        </TranslationGroup>
+      </Wrapper>
     </Container>
   );
 };
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -7,6 +7,49 @@ export const Container = styled.div`
   min-height: 100vh;
 `;
 
-export const TextareaBox = styled.div`
+export const Wrapper = styled.div`
   display: flex;
+  position: relative;
 `;
+
+export const Button = styled.button`
+  border: none;
+  color: #bdbdbd;
+  background-color: #fff;
+  box-shadow: 0 1px 4px 0 rgb(0 0 0 / 37%);
+  font-size: 20px;
+  cursor: pointer;
+  line-height: 1;
+  padding: 10px;
+
+  position: absolute;
+  left: 50%;
+  top: 30px;
+  transform: translateX(-50%);
+  border-radius: 8px;
+
+  z-index: 1;
+
+  > svg {
+    display: block;
+  }
+`;
+
+export const TranslationGroup = styled.div`
+  box-shadow: 0 1px 4px 0 rgb(0 0 0 / 37%);
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  width: 500px;
+  padding: 15px;
+
+  & ~ & {
+    margin-left: 20px;
+  }
+`;
+
+// |--------------------|
+// |          |         |
+// |          |         |
+// |          |         |
+// |--------------------|
